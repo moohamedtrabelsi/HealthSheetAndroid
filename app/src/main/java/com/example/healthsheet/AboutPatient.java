9a07f706dcc2721fec2c4076c34acd1d2cae269a
moohamedtrabelsi <61158524+moohamedtrabelsi@users.noreply.github.com>
2020-12-16 12:20:25 +0100

    about

---
+++ app/src/main/java/com/example/healthsheet/AboutPatient.java
@@ -0,0 +1,14 @@
+package com.example.healthsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_patient);
+    }
+}
