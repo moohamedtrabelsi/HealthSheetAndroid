f32eca1cdf336605287afd35290cdc04cd2fae75
Oumaymakhenine <60826442+Oumaymakhenine@users.noreply.github.com>
2020-12-16 14:22:10 +0100

    update

--- app/src/main/java/com/example/healthsheet/Models/AdpterRecyclerPatient.java
+++ app/src/main/java/com/example/healthsheet/Models/AdpterRecyclerPatient.java
@@ -1,39 +1,62 @@
 package com.example.healthsheet.Models;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.healthsheet.Doctor.PatientListe;
+import com.example.healthsheet.R;
 
 import java.util.ArrayList;
 
-public class AdpterRecyclerPatient extends RecyclerView.Adapter<AdpterRecyler.ViewHolder> {
-    public AdpterRecyclerPatient(PatientListe patientListe, ArrayList<User> users) {
+public class AdpterRecyclerPatient extends RecyclerView.Adapter<AdpterRecyclerPatient.ViewHolder> {
+
+    public AdpterRecyclerPatient(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView FirstName ,LastName ;
+
+        public ViewHolder(View itemView, AdpterRecyclerPatient adpterRecyler) {
+            super(itemView);
+            FirstName = (TextView)itemView.findViewById(R.id.FirstName);
+            LastName = (TextView)itemView.findViewById(R.id.LastName);
+        }
+
+        public void updateholder(User u)
+        {
+            FirstName.setText(u.getFirstName());
+            LastName.setText(u.getLastName());
+        }
+
+    }
+
+    private final ArrayList<User> users;
+    private Context mContext;
+
+    public AdpterRecyclerPatient(Context mContext, ArrayList<User> users) {
+        this.mContext = mContext ;
+        this.users = users;
     }
 
     @NonNull
     @Override
-    public AdpterRecyler.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public AdpterRecyclerPatient.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AdpterRecyler.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull AdpterRecyclerPatient.ViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
         return 0;
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
     }
 }
